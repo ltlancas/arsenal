8ef566d9c051addf860064dcec3535bd19d23bd4
Claude Cournoyer-Cloutier <cournoyc@mcmaster.ca>
2025-01-15 10:49:44 -0500

    Renamed binaries dist func

--- arsenal_gear/dist_funcs/__init__.py
+++ arsenal_gear/dist_funcs/__init__.py
@@ -5,6 +5,6 @@ dist_funcs
 Distribution functions for anything under the sun that arsenal
 needs.
 """
-from . import imf, binary_fraction
+from . import imf, binaries
 
-__all__ = ['imf', 'binary_fraction']
+__all__ = ['imf', 'binaries']
